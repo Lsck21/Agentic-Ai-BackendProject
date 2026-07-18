4aa97cee09c86c4ace601d1d75f776d34cba66a9
Lakhinana Sai Charan Kashyap <Lsckashyap0913@gmail.com>
2026-07-18 23:34:13 +0530

    Update Swagger configuration for Render

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -8,12 +8,13 @@ const options = {
             version: "1.0.0",
             description: "API documentation for Basket Project"
         },
-        servers: [
-            {
-                url: "http://localhost:3000/api"
-            }
-        ],
-
+       servers: [
+    {
+        url: process.env.RENDER_EXTERNAL_URL
+            ? `${process.env.RENDER_EXTERNAL_URL}/api`
+            : "http://localhost:3000/api"
+    }
+]
         components: {
             securitySchemes: {
                 bearerAuth: {
